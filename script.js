77df3b78a5ad14b55103e250d1f4ce918bbf534d
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2025-03-15 22:22:36 -0400

    Changed the secret number to be between 1 and 50

--- script.js
+++ script.js
@@ -7,13 +7,13 @@
 // document.querySelector('.score').textContent = 10;
 
 // document.querySelector('.guess').value = 23;
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 50) + 1;
 let score = 20;
 let highscore = 0;
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  secretNumber = Math.trunc(Math.random() * 50) + 1;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
